55b1341aab439b0f3cf165540c3517a08e50cbde
ARAGAO67 <lucas.p.aragao2@gmail.com>
2026-05-04 15:56:16 +0000

    v2

---
+++ quizz.py
@@ -0,0 +1,30 @@
+print("Quiz futebol⚽")
+
+# pergunta 1
+print("1- quem ganhou a copa de 2002🏆?/n a- alemanha/n b- brasil/n c- argentina")
+r1 = input("resposta: ")
+
+if r1 == "b":
+    print("acertou🎉/n")
+else:
+    print("errou era brasil😣/n")
+
+# pergunta 2
+print("2- quantos jogadores tem no time?/n a- 9/n b- 10/n c- 11")
+r2 = input("resposta: ")
+
+if r2 == "c":
+    print("acertou🎉/n")
+else:
+    print("errou era 11😣/n")
+
+# pergunta 3
+print("3- quem é o rei do futebol?/n a- pele/n b- messi/n c- cristiano ronaldo")
+r3 = input("resposta: ")
+
+if r3 == "a":
+    print("acertou🎉/n")
+else:
+    print("errou era pele😣/n")
+
+print("Parabéns você chegou ao final do quiz🫡")    
