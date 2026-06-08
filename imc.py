5606b40b60d76ec547e211d23fa144fda068a757
ARAGAO67 <lucas.p.aragao2@gmail.com>
2026-06-08 14:29:13 +0000

    v1

---
+++ imc.py
@@ -0,0 +1,30 @@
+#Etapa 1 - Cálculo do IMC
+def calc_imc(imc)
+    imc = peso/ (altura*altura)
+    return imc
+
+#Etapa 2 - Classificação do IMC
+def classificar_imc(resultado):
+    if resultado >= 25:
+        return"ACIMA DO PESO"
+    else:
+        return "PESO NORMAL"
+#Etapa 3 - Mensagem de Retorno
+def mensagem(staus):
+    if status == "ACIMA DO PESO"
+        return "🫷🫸Atenção! procure um médico"
+    else:
+        return "👌Seu peso está normal! continue assim"
+#Etapa 4 - Integração do Código
+valor_peso = float(input("Digite seu peso: "))
+valor_altura = float(input("Digite sua altura: "))
+
+valor_imc = calc_imc(valor_peso,valor_altura)
+resultado_imc = classificar_imc(valor_imc)
+saida = mensagem(resultado_imc)
+
+print("="*50)
+print("Resultado do seu IMC")
+print(f"\n seu IMC é:{valor_imc})
+print(f"\ {saida}")
+print("="*50)
