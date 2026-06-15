2418d8afdb633411dfb25d7b0a1a97aabd910339
ARAGAO67 <lucas.p.aragao2@gmail.com>
2026-06-15 15:54:03 +0000

    v1

---
+++ contagem.py
@@ -0,0 +1,13 @@
+#contador de caracteres - Achando a menor palavra
+def maior_palavra(texto):
+    for p in ",!?;:":
+        texto = texto.replace(p,"")
+    palavra = texto.split()
+    if not palavra:
+        return " "
+    tamanho = ""
+    for frase in palavra:
+       if len(frase) >= len(tamanho):
+        tamanho = frase
+    return tamanho
+print(maior_palavra("meupinto mundo"))
